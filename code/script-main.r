dd1bd3226b00ab30021f99a623f5806ace7e0f7a
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2022-07-23 07:44:25 +0200

    updated

--- code/script-main.r
+++ code/script-main.r
@@ -1,13 +1,13 @@
 # main
 
 ## load libraries
-library (tidyverse)  
-library (lubridate)  #dates
-library (kableExtra) #tables
-library (xtable)     #tables
-library (igraph)
+library(tidyverse)
+library(lubridate)  #dates
+library(kableExtra) #tables
+library(xtable)     #tables
+library(igraph)
 
-## load functions 
+## load functions
 source("./code/fun-labour-market-data.R")
 
 
@@ -17,7 +17,7 @@ path <- "C:/Users/morea/OneDrive - Area Science Park/General - iifvg lavoro/Dati
 #"mese","professione","qualifica","qualifica_codice","rl_ateco","rl_ateco_macro","rl_ateco_settore",
 #"saldo","sede_op_ateco","sede_op_comune","sede_op_indirizzo","sede_op_provincia","somm","tipo_contratto","tipo_orario","cittadinanza","iso3","sigla_prov","comune_istat","codice_istat","SLL_codice","SLL_nome","contratto")
 
-columns = c(
+columns <- c(
     "CF","anno","az_ragione_soc","data","data_fine","data_fine_prev","data_inizio",
     "id_cittadino","data_nascita","eta","genere","cittadinanza","iso3",
     "professione","qualifica","qualifica_codice","contratto",
