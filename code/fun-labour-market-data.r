2ccb9d36017dc5d605004dd1fd8fe73fafedb2e6
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2022-07-23 07:37:06 +0200

    Squashed commit of the following:
    commit 93ea1f8b216397d9d6dfcbe7e05a2a9a714c1547
    Author: Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
    Date:   Sat Jul 23 07:28:16 2022 +0200
    
        small changes
    
    commit 1d18cd3907e9bb0a6b45fa2324e1fb9287e9218f
    Author: Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
    Date:   Sat Jul 23 07:25:22 2022 +0200
    
        small changes
    
    commit 0ba590575dd77faa801a1f4b76fbc0b33ef1de82
    Author: Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
    Date:   Sat Jul 23 07:22:00 2022 +0200
    
        gitignore data results
    
    commit 83e0c933937be76db856b1a9f4950606a16563a8
    Author: Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
    Date:   Sat Jul 23 07:18:44 2022 +0200
    
        load data as a function

--- code/fun-labour-market-data.r
+++ code/fun-labour-market-data.r
@@ -1,5 +1,23 @@
 # functions
 
-load_data <- function(path){
-    return(path)
+# Load data 
+load_data <- function(path, columns){
+    files = list.files(path=path, pattern="dati*", all.files=TRUE, full.names=TRUE)
+    nfiles = length(files)
+    data <- files[1] %>% 
+        read_delim( delim = "|", show_col_types = FALSE) %>%
+        select(columns)  
+
+    for (i in 2:nfiles){
+        print(paste("Loading file ", files[i]))
+        new <- files[i] %>% 
+            read_delim(  delim = "|", show_col_types = FALSE) %>%
+            select(columns) 
+
+        data <- rbind(data,new) 
+        
+         
+    }
+    print(paste(nfiles, "files loaded."))
+    return(data)
 }
--- code/script-main.r
+++ code/script-main.r
@@ -1,6 +1,31 @@
 # main
 
-source("fun-labour-market-data.R")
+## load libraries
+library (tidyverse)  
+library (lubridate)  #dates
+library (kableExtra) #tables
+library (xtable)     #tables
+library (igraph)
 
-tmp <- load_data("X")
-print(tmp)
+## load functions 
+source("./code/fun-labour-market-data.R")
+
+
+path <- "C:/Users/morea/OneDrive - Area Science Park/General - iifvg lavoro/DatiElaborati/Riservati"
+ 
+#columns = c("CF","anno","az_ragione_soc","data","data_fine","data_fine_prev","data_inizio","data_nascita","eta","genere","id_cittadino",
+#"mese","professione","qualifica","qualifica_codice","rl_ateco","rl_ateco_macro","rl_ateco_settore",
+#"saldo","sede_op_ateco","sede_op_comune","sede_op_indirizzo","sede_op_provincia","somm","tipo_contratto","tipo_orario","cittadinanza","iso3","sigla_prov","comune_istat","codice_istat","SLL_codice","SLL_nome","contratto")
+
+columns = c(
+    "CF","anno","az_ragione_soc","data","data_fine","data_fine_prev","data_inizio",
+    "id_cittadino","data_nascita","eta","genere","cittadinanza","iso3",
+    "professione","qualifica","qualifica_codice","contratto",
+    "saldo","sede_op_ateco","sede_op_comune","sede_op_indirizzo","sede_op_provincia",
+    "somm","tipo_contratto","tipo_orario",
+    "sigla_prov","comune_istat","codice_istat","SLL_codice","SLL_nome")
+
+
+data <- load_data(path, columns)  
+print(nrow(data))
+print(ncol(data))
