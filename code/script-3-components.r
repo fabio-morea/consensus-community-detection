bfd0fae344d0eaad8e065b0a48d64b85cedb78bb
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2022-07-27 09:05:58 +0200

    improved plots

---
+++ code/script-3-components.r
@@ -0,0 +1,83 @@
+# Author: Fabio Morea @ Area Science Park
+# Package: Labour Market Network - version 1.0
+# Description: R program to extract information from labour market data.
+#               Original data is not included in the package as it contains persnoal information
+#               Test data is random an contains no personal information
+
+# SPDX-License-Identifier: CC-BY-4.0
+
+# GitLab: https://gitlab.com/fabio-morea-areasciencepark/greencodes 
+
+# script 3: analysing netwoek components
+
+## debug mode
+debug <- FALSE
+echo <- TRUE
+
+## load libraries
+library(tidyverse)
+library(igraph)
+
+## load graph
+print("Loading data...")
+g <- read_graph("./results/full_graph.csv", format="graphml")
+
+windows();plot(g,
+     layout=layout_with_mds,
+     edge.color="gray",
+     edge.size=1,
+     edge.arrow.size = 0.5,
+     vertex.color="black",
+     vertex.size=2,
+     vertex.label=NA)
+ 
+  
+
+
+## identify components
+print("Analysing components...")
+V(g)$comp<-components(g)$membership
+table(components(g)$membership)
+maxcomp<- which.max(table(components(g)$membership))
+
+##plot giant component in red
+print("Plotting full network in separate window...")
+plot_title = "Full network g"
+windows();plot(g,
+             edge.color="gray",
+             edge.arrow.size = 1,
+             vertex.color= if_else ( V(g)$comp == maxcomp,"red","blue" ),
+             vertex.label=NA,
+             vertex.size=5, 
+             layout=layout_with_mds)
+title(main=plot_title,cex.main=1,col.main="black")
+
+
+# plot only giant component
+print("Plotting giant component in separate window...")
+gg <- induced_subgraph(g, V(g)[ V(g)$comp == maxcomp ])
+
+plot_title = "Giant ccomponent "
+windows();plot(gg,
+             edge.color="gray",
+             edge.arrow.size = 0.2,
+             vertex.color= if_else ( V(gg)$comp == maxcomp,"red","blue" ),
+             vertex.label=NA,
+             vertex.size=2, 
+             layout=layout.fruchterman.reingold)
+title(main=plot_title,cex.main=1,col.main="black")
+
+
+print("Plotting other components in separate window...")
+oc <- induced_subgraph(g, V(g)[ V(g)$comp != maxcomp ])
+plot_title = "Other components "
+windows();plot(oc,
+             edge.color="gray",
+             edge.arrow.size = 0.2,
+             vertex.color= if_else ( V(gg)$comp != maxcomp,"red","blue" ),
+             vertex.label=NA,
+             vertex.size=3, 
+             layout=layout.fruchterman.reingold)
+title(main=plot_title,cex.main=1,col.main="black")
+
+print("Script completed.")
