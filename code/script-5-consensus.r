f2b0e0caa74946e657c53bce7b363e0fc093ea8d
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2022-07-31 00:16:13 +0200

    consensus clustering done

--- code/script-5-consensus.r
+++ code/script-5-consensus.r
@@ -10,11 +10,20 @@
 
 # script 5: consensus
 
+A good clusteing algoritm in this domain should identify clusters that
+-	Have at leas min.point 
+-	All trivial clusters of 1 or 2 points are grouped in a cluster 0 FRINGE
+-	Each point is assigned with a PROBABILITY of being part of consensus cluster (as in fuzzy clustering) so we can identify points that are confidently placed in this cluster and points that are not. A result is a matrix of probabilities (N.points X N_clusters)
+We use consensus, similar to random forrest
+-	Small perturbation of algoritm parameters and network (randomly exclude a fraction of elements with low coreness)
+-	Pairwise omparison
+
+
 ## clear terminal
 shell("cls")
 
 ## debug mode
-debug <- FALSE
+debug <- F
 echo <- TRUE
 
 ## load libraries
@@ -28,18 +37,18 @@ source("./code/functions-network-analysis.R")
 print("Loading giant componente and making a graph...")
 gc <- read_graph("./results/giant_component.csv", format="graphml")
 
-if (debug){gc <- induced.subgraph(gc,which(V(gc)$core>5))}
+if (debug){gc <- induced.subgraph(gc,which(V(gc)$core>2))}
 
 # undirected graph to be used for algorithms that do not support directed
 gc_undirected <- as.undirected(gc,mode = "collapse", edge.attr.comb = "sum")
 
+print("consensus clustering...")
 ## CONSENSUS
 res=c(0.90,0.95,1.0,1.05,1.1) 
-n_trials = 200
+n_trials = 500
 cl_louvian_N <- cluster_N_times(g=gc_undirected, 
     res=res,
     n_trials=n_trials, 
-    min_cl_size = 2,
     clustering_algorithm="Louvian")  
 
 as.data.frame(cl_louvian_N,
@@ -69,16 +78,17 @@ for (i in (1:N_trials)){
 }
 
  
+as.data.frame(x)%>%write_csv("./results/x.csv")
 
 x<-x/N_trials#normalize
-print(mean(x))
-windows();heatmap(x)
-windows();hist(x[x>0.0])
+#print(mean(x))
+#windows();heatmap(x)
+#windows();hist(x[x>0.0])
 colnames(x)<-V(gc)$name
 rownames(x)<-V(gc)$name
 
-threshold = .5			# threshold to decide on membership
-relevant_clusters = 0  	# counts only clusters with 3 or more members
+threshold = .5			    # threshold to decide on membership
+current.cluster = 0  	# counts only clusters with 3 or more members
 
 consensus_clusters <- as_tibble_col(rownames(x))%>%
 	mutate(membership=0)
@@ -87,31 +97,59 @@ more_clusers_to_be_found=TRUE
 N_trials   <- ncol(all_clusters)
 
 remaining_prob<-x
+min.points = 4
+
+print("identify clusters above min.points")
+ccs <- tibble(name = "x",mbshp = -1)%>%head(0)
 
 while (more_clusers_to_be_found){
-  
-  print(relevant_clusters)
   cluster_ii_members <- which(remaining_prob[1,] > threshold)
   clust_prob	<- remaining_prob[ cluster_ii_members, cluster_ii_members]
   remaining_prob<- remaining_prob[-cluster_ii_members,-cluster_ii_members]
 
-  if(length(cluster_ii_members)>=2) {
-    relevant_clusters <- relevant_clusters +1
-    consensus_clusters$membership[cluster_ii_members]<- relevant_clusters
-    print(paste("Cluster ", relevant_clusters, " has ", length(cluster_ii_members), " members"))
-	
+  if(length(cluster_ii_members)>=min.points) {
+    current.cluster <- current.cluster + 1
+    for (nn in names(cluster_ii_members)){
+      ccs <- ccs %>% add_row(name=nn , mbshp=current.cluster)
+    }
+    
+    print(paste("Cluster ", current.cluster, " has ", length(cluster_ii_members), " members"))
+	  #print(names(cluster_ii_members))
   }
-  	if ( nrow(remaining_prob)  > 3)  {
-		more_clusers_to_be_found=TRUE
-	} 
-  	else{
-		more_clusers_to_be_found=FALSE
-	}
+  if ( nrow(remaining_prob)  > min.points)  {
+    more_clusers_to_be_found=TRUE
+  } 
+  else{
+    more_clusers_to_be_found=FALSE
+  }
 }
 
-as.data.frame(consensus_clusters)%>%
-    write_csv("./results/clusters_CONSENSUS.csv")
+print("Saving results")
+ccs %>% write_csv("./results/clusters_CONSENSUS.csv")
+#save cluster info in graph structure
+V(gc)$clust_cons <- 0
+for(i in 1:nrow(ccs)) {
+    mmbrsp <- as.numeric(ccs[[i,2]] )
+    nname <- as.character(ccs[[i,1]] )
+    V(gc)[V(gc)$name == nname]$clust_cons <- mmbrsp
+    #print(paste(nname, mmbrsp))
+}
 
-#show_subgraphs (gc_undirected, clusters_membership=consensus_clusters$membership, nrows=2,ncols=4,label="Consensus Louvian" ) 
+clusters_lvC <- make_clusters(
+  gc,
+  membership = V(gc)$clust_cons,
+  algorithm = "louvian consensus",
+  merges = NULL,
+  modularity = FALSE
+)
 
+show_subgraphs (gc_undirected, 
+  clusters_membership = clusters_lvC$membership, 
+  nrows=3,
+  ncols=3,
+  label="Consensus Louvian" ) 
+
+gc %>% write_graph("./results/gc_communities_consensus.csv", format="graphml")
+#as_long_data_frame(gc) %>% write_csv("./results/gc_communities_consensus_as_df.csv")
+ 
 print("Script completed.")
