950fb43e190f378d154a9d892740e32a6a457219
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2022-08-30 15:29:48 +0200

    red vertex plot

--- code/layout_concentric.r
+++ code/layout_concentric.r
@@ -7,6 +7,7 @@
 # libraries
 options(warn=-1)
 library(tidyverse)
+
 library(igraph)
 
 layout_concentric <- function(n,k,r){
@@ -38,39 +39,50 @@ layout_concentric <- function(n,k,r){
 	return(coords)
 }
 
-#coords <- layout_concentric (n = 24, k = 12, r = 3)
+coords <- layout_concentric (n = 24, k = 12, r = 3)
 
-ggplot(layout_dc)+geom_point(aes(x=x,y=y))
+ggplot(coords)+geom_point(aes(x=x,y=y))
 
 clusters_graph <- read_graph("./results/_clusters_graph.csv", format="graphml")
 
-length(V(clusters_graph)$name)
+ 
 
-V(clusters_graph)$core <- graph.coreness(clusters_graph)
-V(clusters_graph)$strength <- strength( clusters_graph, loops = FALSE) 
-edgew <- E(clusters_graph)$weight/max(E(clusters_graph)$weight)*100+1
-edgec <- ifelse(is.loop(clusters_graph), "#5759d800","#07d84dcd")
-edgec <- ifelse(edgew > 6.0, edgec,"#cccccc84")# colour for weak links 
-vertexs <- V(clusters_graph)$strength * 300 
-
-
-
-top_clusters <- V(clusters_graph)$name[1:18]
+top_clusters <- V(clusters_graph)$name[1:30]
 
 # select a number of clusters
-top_clusters <- c("0","1","2","5", "11", "12" ,"13" ,"4","8","16","22","24")
+#top_clusters <- c("0","1","2","5", "11", "12" ,"13" ,"4","8","16","22","24")
 
 ggg <- induced.subgraph(clusters_graph,vids = top_clusters)
+
 coords <- layout_concentric (
 	n = length(V(ggg)$name)-1, 
-	k = 5, 
+	k = 6, 
 	r = 1)
+
+
+red_vertex <- 1
+red_edges <- E(ggg)[from(red_vertex+1) | to(red_vertex+1)]
+edgec <- if_else(E(ggg) %in% red_edges, "red","#bebebe54")  
+
+vertexc <- if_else(V(ggg)$name == red_vertex, "red", "#bebebe1b")
+vertexs <- V(ggg)$strength * 300 
+
+V(ggg)$core <- graph.coreness(ggg)
+V(ggg)$strength <- strength( ggg, loops = FALSE) 
+edgew <- E(ggg)$weight/max(E(ggg)$weight)*10
+# loop color
+# edgec <- if_else(is.loop(clusters_graph), "#5759d800","#07d84dcd")
+# weak edges color
+edgec <- if_else(edgew > .2, edgec,"#cccccc00")# colour for weak links 
+
+
+
 plot(ggg,
 		layout=as.matrix(coords),
 		edge.color=edgec,
 		edge.width=edgew,
 		vertex.size=vertexs,
-		vertex.color = "#04b0ff",
+		vertex.color = vertexc,
 		vertex.label.font=1,
 		vertex.label.color="#3641e1")
 
