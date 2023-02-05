8aadbea2df2b8ebf05617344ec4c3a836f12af08
Fabio Morea <79448231+fabio-morea-areasciencepark@users.noreply.github.com>
2023-02-05 07:23:33 +0100

    first commit

---
+++ params/parameters.r
@@ -0,0 +1,7 @@
+# parameters
+
+# data folder path
+get.data.folder.path <- function(){ 
+    return("C:/Users/morea/OneDrive - Area Science Park/Documenti condivisi/General/DatiElaborati/Riservati")
+}
+
